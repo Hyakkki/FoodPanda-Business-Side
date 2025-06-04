c5c2276546dc6788b29446fbefba516ccc305166
Tristan Jay Sevilla <tristanjaymacsevilla@gmail.com>
2025-06-04 22:28:06 +0800

    Business 06/04/2025
    - Added HomePage class/fxml
    - Added AddProduct class/fxml

---
+++ FoodPanda/src/Business/Class/BusinessAddProductController.java
@@ -0,0 +1,31 @@
+package Business.Class;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class BusinessAddProductController {
+
+    @FXML
+    private TextField productdesctextf;
+
+    @FXML
+    private ImageView productimageview;
+
+    @FXML
+    private TextField productnametextf;
+
+    @FXML
+    private TextField productpricetextf;
+
+    @FXML
+    private TextField productquantitytextf;
+
+    @FXML
+    private Button saveuploadbtn;
+
+    @FXML
+    private Button uploadbtn;
+    
+}
--- FoodPanda/src/Business/Class/BusinessHomePageController.java
+++ FoodPanda/src/Business/Class/BusinessHomePageController.java
@@ -1,5 +1,37 @@
 package Business.Class;
 
+import java.io.IOException;
+
+import Business.SwitchScene;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class BusinessHomePageController {
+
+    @FXML
+    private Button addaproductbtn;
+
+    @FXML
+    private Button homepageaccountbtn;
+
+    @FXML
+    private Button homepagehomebtn;
+
+    @FXML
+    private Label restaunamelabel;
+
+    private Stage stage;
+    private Scene scene; 
+    private Parent root;
+
+    @FXML
+    public void toAddAProductHandler(ActionEvent event) throws IOException{
+        SwitchScene.switchScene(event, "/Business/FXML/BusinessAddProduct.fxml");
+    }
     
 }
